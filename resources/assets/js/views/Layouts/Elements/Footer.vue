87ce1acad9482c17a32a50275d1ff96048587518
Lauro Guedes <lauro.guedes@eagletecnologia.com.br>
2018-10-17 18:01:44 -0300

    Inserção das logos base e estilização links texto sem componente

--- resources/assets/js/views/Layouts/Elements/Footer.vue
+++ resources/assets/js/views/Layouts/Elements/Footer.vue
@@ -1,12 +1,29 @@
 <template>
-    <el-row class="h-100 sm-text" type="flex" align="middle">
-        <el-col :span="18">Laravel project with VueJS Single Page Application</el-col>
-        <el-col class="right-align" :span="6">Logo</el-col>
+    <el-row class="footer-content h-100 sm-text" type="flex" align="middle">
+        <el-col :span="18">Laravel + VueJs SPA + Element UI by <a href="https://leowgweb.com.br" class="link-white" target="_blank">leowgweb</a></el-col>
+        <el-col class="right-align" :span="6">
+            <img :src="getLogo" :alt="getName">
+        </el-col>
     </el-row>
 </template>
 
-<style></style>
+<style lang="scss">
+    .footer-content{
+        img{
+            max-height: 25px;
+        }
+    }
+</style>
 
 <script type="text/javascript">
-    export default {}
+    export default {
+        computed: {
+            getLogo(){
+                return `${window.location.origin}/img/simbol-element-white.svg`
+            },
+            getName(){
+                return 'Logo Element'
+            }
+        }
+    }
 </script>
--- resources/assets/js/views/Layouts/Elements/Header.vue
+++ resources/assets/js/views/Layouts/Elements/Header.vue
@@ -1,9 +1,34 @@
 <template>
-    <div class="header-content">Header | <router-link :to="{ name: 'page-one' }">Página 1</router-link> | <router-link :to="{ name: 'page-two' }">Página 2</router-link> | <router-link :to="{ name: 'page-three' }">Página 3</router-link></div>
+    <el-row class="header-content h-100" type="flex" align="middle">
+        <el-col :span="18">
+            <img :src="getLogo" :alt="getName">
+        </el-col>
+        <el-col class="right-align" :span="6">
+            <el-badge :value="5">
+                <el-button icon="el-icon-bell" circle></el-button>
+            </el-badge>
+        </el-col>
+    </el-row>
 </template>
 
-<style></style>
+<style lang="scss">
+    .header-content{
+        .el-badge__content.is-fixed{
+            top: 5px;
+            right: 15px;
+        }
+    }
+</style>
 
 <script type="text/javascript">
-    export default {}
+    export default {
+        computed: {
+            getLogo(){
+                return `${window.location.origin}/img/logo-element-white.svg`
+            },
+            getName(){
+                return 'Logo Element'
+            }
+        }
+    }
 </script>
